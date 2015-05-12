ea174ecfce94a9b1125a6d6f231d333a355caf8f
Sterling Smart <sterling.smart@infospace.com>
2015-05-11 17:57:00 -0700

    Simplify serializer
    Just use JSON.NET and don't use the RendererMap as a fallback strategy.

--- shared/JsonDotNetSerializer.cs
+++ shared/JsonDotNetSerializer.cs
@@ -1,35 +1,23 @@
-using System.IO;
-using System.Text;
 using log4net.ObjectRenderer;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace log4net.Util.Serializer
 {
-    public class JsonDotNetSerializer : ISerializer
+    public class JsonDotNetSerializer : ISerializer, ISerializerFactory
     {
-        public RendererMap Map { get; set; }
 
         public object Serialize(object obj)
         {
-            object result;
-
-            if (obj != null)
+            var settings = new JsonSerializerSettings
             {
-                using (var writer = new StringWriter(new StringBuilder()))
-                using (var jsonWriter = new RendererMapJsonTextWriter(Map, writer) { CloseOutput = false })
-                {
-                    jsonWriter.WriteValue(obj);
+            };
 
-                    result = writer.GetStringBuilder().ToString();
-                }
-            }
-            else
-            {
-                result = JsonConvert.SerializeObject(obj);
-            }
+            return JsonConvert.SerializeObject(obj, settings);
+        }
 
-            return result;
+        public ISerializer GetSerializer(object obj, RendererMap map)
+        {
+            return this;
         }
     }
 }
